82924b62481b10eac9f1c97b6f7c188dcae19ff7
roaris <one0803kyopro@gmail.com>
2023-04-23 17:55:17 +0900

    add vscode settings

--- setup.sh
+++ setup.sh
@@ -23,3 +23,16 @@ do
 done < ./brew/brew_cask
 
 ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
+
+ln -s ~/dotfiles/vscode/settings.json ~/Library/Application Support/Code/User/settings.json
+
+echo "Installing VSCode Extensions..."
+
+if [ -e /usr/local/bin/code ]; then
+    while read line
+    do
+        code --install-extension $line
+    done < ./vscode/extensions
+else
+    echo "Install the code command from the command palette to add your extensions."
+fi
